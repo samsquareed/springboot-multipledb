f3ea22b8e2f3744f086f923791103bb8f55cae70
= <=>
2022-03-29 16:19:04 +0530

    configurations of primary datasource

--- src/main/java/com/datagrokr/multitenant/configuration/PrimaryDBConfiguration.java
+++ src/main/java/com/datagrokr/multitenant/configuration/PrimaryDBConfiguration.java
@@ -1,8 +1,59 @@
 package com.datagrokr.multitenant.configuration;
 
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import org.hibernate.jpa.boot.spi.EntityManagerFactoryBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
+@EnableJpaRepositories(
+    basePackages = "com.datagrokr.multitenant.repository.primary",
+    entityManagerFactoryRef = "primaryEntityManagerFactory",
+    transactionManagerRef = "primaryTransactionManager"
+)
 public class PrimaryDBConfiguration {
     
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource.primary")
+    public DataSourceProperties primaryDataSourceProperties(){
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource.primary.configuration")
+    public DataSource primaryDataSource(){
+        return primaryDataSourceProperties().initializeDataSourceBuilder()
+            .type(HikariDataSource.class).build();
+    }
+
+    @Primary
+    @Bean(name = "primaryEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean primaryEntityManagerFactory(org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder builder,
+            @Qualifier("primaryDataSource") DataSource primaryDataSource) {
+        return builder
+                .dataSource(primaryDataSource())
+                .packages("com.datagrokr.multitenant.entity.primary")
+                .build();
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager primaryTransactionManager(
+            final @Qualifier("primaryEntityManagerFactory") LocalContainerEntityManagerFactoryBean primaryEntityManagerFactory) {
+        return new JpaTransactionManager(primaryEntityManagerFactory.getObject());
+    }
+
 }
