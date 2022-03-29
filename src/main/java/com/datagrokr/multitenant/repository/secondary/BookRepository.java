5e8ccb03db98ca26473d7b0b5b52ac0e1fcf72ac
= <=>
2022-03-29 17:53:48 +0530

    solved : small typo error misplaced the Databse of primary in secondary

--- src/main/java/com/datagrokr/multitenant/repository/secondary/BookRepository.java
+++ src/main/java/com/datagrokr/multitenant/repository/secondary/BookRepository.java
@@ -1,11 +1,11 @@
 package com.datagrokr.multitenant.repository.secondary;
 
-import com.datagrokr.multitenant.entity.primary.PrimaryBook;
+import com.datagrokr.multitenant.entity.secondary.Book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends JpaRepository<PrimaryBook, Integer> {
+public interface BookRepository extends JpaRepository<Book, Integer> {
 
 }
